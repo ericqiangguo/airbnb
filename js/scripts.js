e5c241ed1af120177b6880dd58c9a5b91c9e9511
Boxin Lyu <b5lyu@uwaterloo.ca>
2019-03-02 23:16:20 -0500

    search house with google map

--- js/scripts.js
+++ js/scripts.js
@@ -27,6 +27,7 @@ form_login.onsubmit = function(e) {
     },
     success: function(msg) {
       console.log(msg);
+      window.location.href='../addroom.html';
     },
     fail: function() {
       console.log("login failed");
