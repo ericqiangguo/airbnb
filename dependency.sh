87da2d6cfdddf5f603d98de71cfd07e36c01b560
Boxin Lyu <b5lyu@uwaterloo.ca>
2019-03-18 15:59:37 -0400

    change version of webdriver

--- dependency.sh
+++ dependency.sh
@@ -1,6 +1,6 @@
 mkdir driver
 cd driver
-wget https://chromedriver.storage.googleapis.com/2.41/chromedriver_linux64.zip
+wget https://chromedriver.storage.googleapis.com/74.0.3729.6/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
 echo `pwd`
 ls
