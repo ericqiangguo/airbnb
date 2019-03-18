43aea01006212d0ea67e9e25b2cf5f689f36759e
Boxin Lyu <b5lyu@uwaterloo.ca>
2019-03-18 15:41:33 -0400

    modify

--- dependency.sh
+++ dependency.sh
@@ -2,4 +2,6 @@ mkdir /driver
 cd /driver
 wget https://chromedriver.storage.googleapis.com/2.41/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
+echo `pwd`
+ls
 
--- test.sh
+++ test.sh
@@ -1,3 +1,7 @@
 export PATH=/driver:$PATH
 
+echo $PATH
+
+ls /driver
+
 python test.py
