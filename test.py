352f422ff0565ff29515b6a38696f620283bb4f4
Boxin Lyu <b5lyu@uwaterloo.ca>
2019-03-18 18:35:12 -0400

    modify test.sh and test.py

--- test.py
+++ test.py
@@ -3,26 +3,18 @@ import re
 from splinter.browser import Browser
 
 CLOASE_AFTER_TEST =False
-GBK = "gbk"
-UTF8 = "utf8"
-
-reload(sys)
-sys.setdefaultencoding(UTF8)
-
-encoding = lambda x:x.encode('gbk')
 
 def output(x):
-    print encoding(x)
+    print x
 
 def resultMsg(x):
         assert x==True
         print 'pass'
-    # print '---------------------'
 
 def testLogin(desc, username, password, result):
     output(desc)
-    browser.fill('email',username.decode(UTF8))
-    browser.fill('pass',password.decode(UTF8))
+    browser.fill('email',username)
+    browser.fill('pass',password)
     browser.find_by_value('login').first.click()
     checkresult(result)
 
@@ -34,11 +26,11 @@ __testUrl = 'http://ubuntu1604-006.student.cs.uwaterloo.ca:19487/'
 browser = Browser('chrome',headless=True)
 browser.visit(__testUrl)
 
-output("test page:"+ browser.title)
+output('test page:'+ browser.title)
 
 
 
-testLogin("no username",'','','please enter your email')
+testLogin('no username','','','please enter your email')
 testLogin('no password','lyuboxin@gmail','','please enter your password')
 testLogin('unexist username','lyuboxin1@gmail','','username or password is wrong')
 
