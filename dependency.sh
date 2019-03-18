34cfb767177ee27b904a8ccf0097264ba7c8444d
Boxin Lyu <b5lyu@uwaterloo.ca>
2019-03-18 16:18:12 -0400

    change driver version to 73

--- dependency.sh
+++ dependency.sh
@@ -1,6 +1,6 @@
 mkdir driver
 cd driver
-wget https://chromedriver.storage.googleapis.com/74.0.3729.6/chromedriver_linux64.zip
+wget https://chromedriver.storage.googleapis.com/73.0.3683.68/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
 echo `pwd`
 ls
